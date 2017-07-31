40a5f93ffe4f4d562b185491c5a0081ba8461423
nmcapule <llss.s.sls.lll@gmail.com>
2017-08-01 03:27:45 +0800

    Add users syncing

--- index.js
+++ index.js
@@ -1,30 +1,89 @@
 const firebase = require("firebase-admin");
-
-const serviceAccount = require("./visitour-admin.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://visitour-2a6d8.firebaseio.com"
-});
-
-console.log("connecting...");
-let db = firebase.database();
-
-let userRef = db.ref("user");
-userRef.once("value", (snapshot) => {
-  console.log(snapshot.val());
-});
-
-userRef.on("child_changed", (snapshot) => {
-  let value = snapshot.val();
-  console.log(snapshot.key);
-  console.log(value);
-});
-
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "visitour"
-});
+function firebaseInit() {
+  const serviceAccount = require("./visitour-admin.json");
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://visitour-2a6d8.firebaseio.com"
+  });
+
+  return firebase;
+}
+
+function mysqlInit() {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "visitour"
+  });
+
+  return connection;
+}
+
+function setupSyncingUsers(firebaseDb, mysqlDb) {
+  // Clear users.
+  console.log("Resetting table user...");
+  mysqlDb.query("TRUNCATE TABLE user", (error, results, fields) => {
+    if (error !== null) console.error(error);
+  });
+
+  const insertUser = (id, user) => {
+    const { email, avatar, name } = user;
+    const dbUser = { id, email, avatar, name };
+
+    mysqlDb.query("INSERT INTO user SET ?", dbUser, (error, results, fields) => {
+      if (error !== null) console.error(error);
+    });
+  };
+
+  const updateUser = (id, user) => {
+    const { email, avatar, name } = user;
+    const dbUser = { id, email, avatar, name };
+
+    mysqlDb.query("UPDATE user SET ? WHERE id = ?", [dbUser, id], (error, results, fields) => {
+      if (error !== null) console.error(error);
+    });
+  }
+
+  // Update users with new data.
+  console.log("connecting to firebase...");
+  const userRef = firebaseDb.ref("user");
+
+  console.log("listing users...");
+  userRef.once("value", (snapshot) => {
+    const users = snapshot.val();
+
+    Object.keys(users).map((key) => {
+      const user = users[key];
+      console.log("inserting user: " + user.name);
+      insertUser(key, user);
+    });
+
+    console.log("done");
+
+  });
+
+  userRef.on("child_changed", (snapshot) => {
+    const key = snapshot.key;
+    const user = snapshot.val();
+
+    console.log("updating user: " + user.name);
+    updateUser(key, user);
+    console.log("done");
+  });
+}
+
+function main() {
+  const firebaseDb = firebaseInit().database();
+
+  const mysqlDb = mysqlInit();
+  mysqlDb.connect();
+
+  setupSyncingUsers(firebaseDb, mysqlDb);
+
+  // conn.end();
+}
+
+main();
