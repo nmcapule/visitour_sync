2d5429c59b208b969dca0c20f75a4a8abf4ad02a
nmcapule <llss.s.sls.lll@gmail.com>
2017-08-01 12:15:01 +0800

    move sync users setup outside of index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const firebase = require("firebase-admin");
 const mysql = require("mysql");
+const syncUsers = require("./lib/sync_users.js");
 
 function firebaseInit() {
   const serviceAccount = require("./credentials-firebase.json");
@@ -19,66 +20,15 @@ function mysqlInit() {
   return connection;
 }
 
-function setupSyncingUsers(firebaseDb, mysqlDb) {
-  // Clear users.
-  console.log("Resetting table user...");
-  mysqlDb.query("TRUNCATE TABLE user", (error, results, fields) => {
-    if (error !== null) console.error(error);
-  });
-
-  const insertUser = (id, user) => {
-    const { email, avatar, name } = user;
-    const dbUser = { id, email, avatar, name };
-
-    mysqlDb.query("INSERT INTO user SET ?", dbUser, (error, results, fields) => {
-      if (error !== null) console.error(error);
-    });
-  };
-
-  const updateUser = (id, user) => {
-    const { email, avatar, name } = user;
-    const dbUser = { id, email, avatar, name };
-
-    mysqlDb.query("UPDATE user SET ? WHERE id = ?", [dbUser, id], (error, results, fields) => {
-      if (error !== null) console.error(error);
-    });
-  }
-
-  // Update users with new data.
-  console.log("connecting to firebase...");
-  const userRef = firebaseDb.ref("user");
-
-  console.log("listing users...");
-  userRef.once("value", (snapshot) => {
-    const users = snapshot.val();
-
-    Object.keys(users).map((key) => {
-      const user = users[key];
-      console.log("inserting user: " + user.name);
-      insertUser(key, user);
-    });
-
-    console.log("done");
-
-  });
-
-  userRef.on("child_changed", (snapshot) => {
-    const key = snapshot.key;
-    const user = snapshot.val();
-
-    console.log("updating user: " + user.name);
-    updateUser(key, user);
-    console.log("done");
-  });
-}
-
 function main() {
   const firebaseDb = firebaseInit().database();
 
   const mysqlDb = mysqlInit();
   mysqlDb.connect();
 
-  setupSyncingUsers(firebaseDb, mysqlDb);
+  console.log(syncUsers);
+
+  syncUsers.setup(firebaseDb, mysqlDb);
 
   // conn.end();
 }
